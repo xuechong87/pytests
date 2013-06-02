f03186f505eada94a9cdcb00d10dd3ef735c4ac8
xuechong87 <xuechong87@gmail.com>
2013-06-02 22:19:50 +0800

    dict

--- py27tests/src/er.py
+++ py27tests/src/er.py
@@ -3,7 +3,33 @@ Created on 2013-6-2
 
 @author: xuechong
 '''
-if __name__=="__main__":
+
+    
+def dictionaryTest():
+    '''
+    dictionary tests
+    '''
     dictionary = {"server":"mpilgrim","database":"master"}
     print dictionary["server"]
     print dictionary["database"]
+    dictionary["uid"]="sa"#add 
+    
+    print dictionary["uid"]
+    dictionary["Uid"]="sss"#case sensitive
+    print dictionary["Uid"]
+    
+    print dictionary
+    del dictionary["Uid"]#delete
+    print dictionary
+        
+    dictionary.clear()
+    print dictionary
+        
+if __name__=="__main__":
+    print dictionaryTest.__doc__
+    dictionaryTest()
+    
+    
+    
+    
+    
