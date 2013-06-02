9f5a3f81912097e2da8890c7ec38021d469a8944
xuechong87 <xuechong87@gmail.com>
2013-06-02 21:47:47 +0800

    dic

---
+++ py27tests/src/er.py
@@ -0,0 +1,9 @@
+'''
+Created on 2013-6-2
+
+@author: xuechong
+'''
+if __name__=="__main__":
+    dictionary = {"server":"mpilgrim","database":"master"}
+    print dictionary["server"]
+    print dictionary["database"]
