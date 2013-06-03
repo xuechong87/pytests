ea556454290dd6ffa801fdfe7c16d26ebe886323
xuechong87 <xuechong87@gmail.com>
2013-06-03 21:56:00 +0800

    list

--- py27tests/src/lists.py
+++ py27tests/src/lists.py
@@ -8,7 +8,6 @@ def test():
     listSearch()
     return
 
-
 def listSearch():
     '''search in list'''
     print(listSearch.__doc__)
@@ -17,9 +16,27 @@ def listSearch():
     try:
         print str(list_.index("x"))
     except:
-        print('exception when not found\r\n and this still happens in py3.x')
-    
+        print('exception when not found\r\nand this still happens in py3.x')
+    else:
+        print('some thing is really wrong')
+        
+    list_.remove("c")#remove a obj
+    try:
+        list_.remove("c")
+    except:
+        print('will throw ex when no such obj in the list')
+        
+def listOp():
+    li_ = list(range(0,9))
+    print (li_)
+    li_= [ele*2+ele/3 for ele in li_]
+    print li_
+    return
 
 if __name__=="__main__":
     print (__doc__)
     test()
+    print(str(listOp()))
+    
+    
+    
